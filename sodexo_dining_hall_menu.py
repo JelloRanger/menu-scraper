ee0e63a75bef3dbaec2108a10f03391be714be74
hydroArgentum <baekj@rpi.edu>
2014-06-20 20:11:39 -0400

    Sodexo Dining Hall Menu Initial Commit
    Add the descriptions for the Sodexo Dining Hall Menu (re-written code).

---
+++ sodexo_dining_hall_menu.py
@@ -0,0 +1,11 @@
+"""Name:     Dining Hall Menu.
+File name:   dining_hall_menu.py
+Description: Controls what sites we are accessing and where the output
+             is sent.
+Content:
+         Classes:
+		          DiningHallMenu
+		 "Main Code"
+Required libraries:
+                    Sodexo Menu Parser
+"""
