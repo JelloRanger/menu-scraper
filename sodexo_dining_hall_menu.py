68e7871e33f31ddd762d4a7b4437f3e1e690ea18
hydroArgentum <baekj@rpi.edu>
2014-06-24 16:22:05 -0400

    Dining Hall Menu Initialization
    Adds code that is used to initialize the Dining Hall Menu class.

--- sodexo_dining_hall_menu.py
+++ sodexo_dining_hall_menu.py
@@ -4,8 +4,33 @@ Description: Controls what sites we are accessing and where the output
              is sent.
 Content:
          Classes:
-		          DiningHallMenu
-		 "Main Code"
+                  DiningHallMenu
+         "Main Code"
 Required libraries:
                     Sodexo Menu Parser
 """
+
+import sodexo_parser
+
+class DiningHallMenu():
+    """Dining Hall Menu Class.
+    Here, we define the parser's default HTML URLs that the class
+    searches.
+    The current default is the RPI Sodexo Menu URLs.
+    """
+
+    def __init__(self):
+    """Sodexo Dining Hall Menu Constructor
+    Here, we define our default parser and default URLs.
+    
+    Arguments: self - Used to reference parent class.
+    """
+        self.parser = sodexo_parser.MenuParser()
+        self.urls = {
+                "Commons":  "http://rpi.sodexomyway.com/Menu/Commons1.htm",
+                "Commons2": "http://rpi.sodexomyway.com/Menu/Commons2.htm",
+                "Sage":     "http://rpi.sodexomyway.com/Menu/Sage.htm",
+                "Sage2":    "http://rpi.sodexomyway.com/Menu/Sage2.htm",
+                "BARH":     "http://rpi.sodexomyway.com/Menu/BARH.htm",
+                "BARH2":    "http://rpi.sodexomyway.com/Menu/BARH2.htm",
+                "Blitman":  "http://rpi.sodexomyway.com/Menu/Blitmans.htm"}
